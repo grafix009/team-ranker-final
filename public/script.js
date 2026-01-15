90ef7d1d06c09cb82eb968be3cae0d1d87b895a0
GT5 <gillytovar@gmail.com>
2026-01-15 16:46:39 -0600

    Initial commit of final

---
+++ public/script.js
@@ -0,0 +1,106 @@
+
+const protocol = location.protocol === "https:" ? "wss" : "ws";
+const socket = new WebSocket(`${protocol}://${location.host}`);
+const lists = document.getElementById("lists");
+
+socket.onmessage = e => {
+  const d = JSON.parse(e.data);
+  if (d.teams) render(d.teams);
+};
+
+function render(teams) {
+  lists.innerHTML = "";
+  for (const team in teams) {
+    const div = document.createElement("div");
+    div.className = "team";
+    div.innerHTML = `<h4>${team}</h4>
+      <button onclick="renameTeam('${team}')">Rename</button>
+      <button onclick="deleteTeam('${team}')">Delete</button>`;
+
+    const ul = document.createElement("ul");
+    let dragIndex = null;
+
+    teams[team].forEach((item, idx) => {
+      const li = document.createElement("li");
+      li.textContent = `${idx+1}. ${item.text}`;
+      li.draggable = true;
+
+      li.ondragstart = e => dragIndex = idx;
+
+      ul.appendChild(li);
+    });
+
+    ul.ondragover = e => e.preventDefault();
+
+    ul.ondrop = e => {
+      const rect = ul.getBoundingClientRect();
+      const y = e.clientY - rect.top;
+      let to = teams[team].length - 1;
+      let h = 0;
+
+      [...ul.children].forEach((li,i)=>{
+        h += li.offsetHeight;
+        if (y < h/2) to = i;
+      });
+
+      const arr = [...teams[team]];
+      const moved = arr.splice(dragIndex,1)[0];
+      arr.splice(to,0,moved);
+      socket.send(JSON.stringify({type:"reorder",team,items:arr}));
+    };
+
+    div.appendChild(ul);
+    lists.appendChild(div);
+  }
+}
+
+function addItem() {
+  socket.send(JSON.stringify({
+    type:"addItem",
+    item:{ text:text.value, number:num.value }
+  }));
+}
+
+function addTeam() {
+  socket.send(JSON.stringify({ type:"addTeam", name:teamName.value }));
+}
+
+function renameTeam(name) {
+  const n = prompt("New name", name);
+  if(n) socket.send(JSON.stringify({ type:"renameTeam", oldName:name, newName:n }));
+}
+
+function deleteTeam(name) {
+  if(confirm("Delete "+name+"?"))
+    socket.send(JSON.stringify({ type:"deleteTeam", name }));
+}
+
+function exportCSV() {
+  let csv = "";
+  Object.entries(window.lastTeams || {}).forEach(([t,items])=>{
+    csv += t + "\n";
+    items.forEach((i,x)=> csv += `${x+1},${i.text}\n`);
+    csv += "\n";
+  });
+  const a = document.createElement("a");
+  a.href = URL.createObjectURL(new Blob([csv]));
+  a.download = "rankings.csv";
+  a.click();
+}
+
+function importCSV(e) {
+  const r = new FileReader();
+  r.onload = () => {
+    const lines = r.result.split("\n");
+    let teams={}, current=null;
+    lines.forEach(l=>{
+      if(!l.includes(",")) current=l.trim(), teams[current]=[];
+      else {
+        const [,text] = l.split(",");
+        teams[current].push({text});
+      }
+    });
+    socket.send(JSON.stringify({ type:"import", teams }));
+  };
+  r.readAsText(e.target.files[0]);
+}
---
+++ server.js
@@ -0,0 +1,64 @@
+
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const DATA_FILE = path.join(__dirname, "data", "rankings.json");
+let teams = {};
+
+function load() {
+  if (fs.existsSync(DATA_FILE)) {
+    teams = JSON.parse(fs.readFileSync(DATA_FILE));
+  } else {
+    teams = { "Team A": [], "Team B": [] };
+    save();
+  }
+}
+
+function save() {
+  fs.writeFileSync(DATA_FILE, JSON.stringify(teams, null, 2));
+}
+
+load();
+app.use(express.static("public"));
+
+wss.on("connection", ws => {
+  ws.send(JSON.stringify({ type: "init", teams }));
+
+  ws.on("message", msg => {
+    const d = JSON.parse(msg);
+
+    if (d.type === "addItem")
+      Object.values(teams).forEach(t => t.push(d.item));
+
+    if (d.type === "reorder")
+      teams[d.team] = d.items;
+
+    if (d.type === "addTeam") {
+      const first = Object.keys(teams)[0];
+      teams[d.name] = first ? JSON.parse(JSON.stringify(teams[first])) : [];
+    }
+
+    if (d.type === "renameTeam") {
+      teams[d.newName] = teams[d.oldName];
+      delete teams[d.oldName];
+    }
+
+    if (d.type === "deleteTeam")
+      delete teams[d.name];
+
+    if (d.type === "import")
+      teams = d.teams;
+
+    save();
+    wss.clients.forEach(c => c.send(JSON.stringify({ type: "update", teams })));
+  });
+});
+
+server.listen(process.env.PORT || 3000);
